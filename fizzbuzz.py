2f44a70b2f3151df800bd27a5eed6b901add2602
Joe Nadeau <jgn81669@gmail.com>
2019-11-22 16:05:33 -0600

    First Commit

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+# Author is John Elder for Codemy.com
+# Fizzbuzz
+# 11/22/2019
+
+num = 1
+
+while (num <= 100):
+    if (num % 3 == 0) and (num % 5 == 0):
+        print(str(num) + ". Fizzbuzz!")
+    elif (num % 3 == 0):
+        print(str(num) + ". Fizz!")
+    elif (num % 5 == 0):
+        print (str(num) + ". Buzz!")
+    else:
+        print(str(num) + ".")
+
+    num += 1
