deb13e328236f60e25c63ebaf843fc14a1d97492
Joe Nadeau <jgn81669@gmail.com>
2019-11-22 16:39:17 -0600

    Added report at end.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,16 +2,30 @@
 # Fizzbuzz
 # 11/22/2019
 
-num = 1
+num = 1 # varible to count 1 to 100
+fizzbuzzcount = 0 # Counter Varible
+fizzcount = 0
+buzzcount = 0
 
 while (num <= 100):
-    if (num % 3 == 0) and (num % 5 == 0):
+    if (num % 3 == 0) and (num % 5 == 0): #Using modulus to get the numbers that 3 and 5 will divide into evenly.
         print(str(num) + ". Fizzbuzz!")
+        fizzbuzzcount += 1
+
     elif (num % 3 == 0):
         print(str(num) + ". Fizz!")
+        fizzcount += 1
+
     elif (num % 5 == 0):
         print (str(num) + ". Buzz!")
+        buzzcount += 1
+
     else:
         print(str(num) + ".")
 
     num += 1
+
+print("*******************************************")
+print("Fizz\tBuzz\tFizzbuzz")
+print(str(fizzcount) + "\t" + str(buzzcount) + "\t" + str(fizzbuzzcount))
+print("*******************************************")
